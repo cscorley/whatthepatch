aa7a41676162e46949e922df6c6d1b938c509bd8
Christopher Corley <cscorley@ua.edu>
2012-12-26 16:47:08 -0600

    Add setup.py, autoenv .env, .gitignore files

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='whatthepatch',
+    version='0.0.1',
+    description='A patch parsing library',
+    long_description=readme,
+    author='Christopher S. Corley',
+    author_email='cscorley@ua.edu',
+    url='https://github.com/cscorley/whatthepatch',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
