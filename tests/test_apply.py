70060d7d8d91b49a64ebb9496e62b65195f34b05
Christopher Corley <cscorley@gmail.com>
2019-07-20 15:51:06 -0400

    Prep 6 release (#24)
    * prep 6 release
    
    * force urllib3 update
    
    * add setuptools and wheel to dev packages

--- tests/test_apply.py
+++ tests/test_apply.py
@@ -20,10 +20,10 @@ class ApplyTestSuite(unittest.TestCase):
     """Basic test cases."""
 
     def setUp(self):
-        with open('tests/casefiles/lao') as f:
+        with open("tests/casefiles/lao") as f:
             self.lao = f.read().splitlines()
 
-        with open('tests/casefiles/tzu') as f:
+        with open("tests/casefiles/tzu") as f:
             self.tzu = f.read().splitlines()
 
     def test_truth(self):
@@ -33,28 +33,28 @@ class ApplyTestSuite(unittest.TestCase):
         self.assertEqual(len(self.tzu), 13)
 
     def test_diff_default(self):
-        with open('tests/casefiles/diff-default.diff') as f:
+        with open("tests/casefiles/diff-default.diff") as f:
             diff_text = f.read()
 
         self.assertEqual(_apply(self.lao, diff_text), self.tzu)
         self.assertEqual(_apply_r(self.tzu, diff_text), self.lao)
 
     def test_diff_context(self):
-        with open('tests/casefiles/diff-context.diff') as f:
+        with open("tests/casefiles/diff-context.diff") as f:
             diff_text = f.read()
 
         self.assertEqual(_apply(self.lao, diff_text), self.tzu)
         self.assertEqual(_apply_r(self.tzu, diff_text), self.lao)
 
     def test_diff_unified(self):
-        with open('tests/casefiles/diff-unified.diff') as f:
+        with open("tests/casefiles/diff-unified.diff") as f:
             diff_text = f.read()
 
         self.assertEqual(_apply(self.lao, diff_text), self.tzu)
         self.assertEqual(_apply_r(self.tzu, diff_text), self.lao)
 
     def test_diff_unified_bad(self):
-        with open('tests/casefiles/diff-unified-bad.diff') as f:
+        with open("tests/casefiles/diff-unified-bad.diff") as f:
             diff_text = f.read()
 
         with assert_raises(exceptions.ApplyException) as ec:
@@ -62,13 +62,13 @@ class ApplyTestSuite(unittest.TestCase):
 
         e = ec.exception
         e_str = str(e)
-        assert 'line 4' in e_str
-        assert 'The Named is the mother of all tings.' in e_str
-        assert 'The Named is the mother of all things.' in e_str
+        assert "line 4" in e_str
+        assert "The Named is the mother of all tings." in e_str
+        assert "The Named is the mother of all things." in e_str
         assert e.hunk == 1
 
     def test_diff_unified_bad2(self):
-        with open('tests/casefiles/diff-unified-bad2.diff') as f:
+        with open("tests/casefiles/diff-unified-bad2.diff") as f:
             diff_text = f.read()
 
         with assert_raises(exceptions.ApplyException) as ec:
@@ -76,13 +76,13 @@ class ApplyTestSuite(unittest.TestCase):
 
         e = ec.exception
         e_str = str(e)
-        assert 'line 9' in e_str
-        assert 'The two are te same,' in e_str
-        assert 'The two are the same,' in e_str
+        assert "line 9" in e_str
+        assert "The two are te same," in e_str
+        assert "The two are the same," in e_str
         assert e.hunk == 2
 
     def test_diff_unified_bad_backward(self):
-        with open('tests/casefiles/diff-unified-bad2.diff') as f:
+        with open("tests/casefiles/diff-unified-bad2.diff") as f:
             diff_text = f.read()
 
         with assert_raises(exceptions.ApplyException) as ec:
@@ -90,46 +90,42 @@ class ApplyTestSuite(unittest.TestCase):
 
         e = ec.exception
         e_str = str(e)
-        assert 'line 1' in e_str
-        assert 'The Way that can be told of is not the eternal Way;' in e_str
-        assert 'The Nameless is the origin of Heaven and Earth;' in e_str
+        assert "line 1" in e_str
+        assert "The Way that can be told of is not the eternal Way;" in e_str
+        assert "The Nameless is the origin of Heaven and Earth;" in e_str
         assert e.hunk == 1
 
     def test_diff_unified_bad_empty_source(self):
-        with open('tests/casefiles/diff-unified-bad2.diff') as f:
+        with open("tests/casefiles/diff-unified-bad2.diff") as f:
             diff_text = f.read()
 
         with assert_raises(exceptions.ApplyException) as ec:
-            _apply('', diff_text)
+            _apply("", diff_text)
 
         e = ec.exception
         e_str = str(e)
-        assert 'line 1' in e_str
-        assert 'The Way that can be told of is not the eternal Way;' in e_str
-        assert 'does not exist in source'
+        assert "line 1" in e_str
+        assert "The Way that can be told of is not the eternal Way;" in e_str
+        assert "does not exist in source"
         assert e.hunk == 1
 
     def test_diff_unified_patchutil(self):
-        with open('tests/casefiles/diff-unified.diff') as f:
+        with open("tests/casefiles/diff-unified.diff") as f:
             diff_text = f.read()
 
+        self.assertEqual(_apply(self.lao, diff_text, use_patch=True), (self.tzu, None))
         self.assertEqual(
-            _apply(self.lao, diff_text, use_patch=True),
-            (self.tzu, None),
-        )
-        self.assertEqual(
-            _apply_r(self.tzu, diff_text, use_patch=True),
-            (self.lao, None),
+            _apply_r(self.tzu, diff_text, use_patch=True), (self.lao, None)
         )
 
         new_text = _apply(self.lao, diff_text, use_patch=True)
         self.assertEqual(new_text, (self.tzu, None))
 
         with assert_raises(exceptions.ApplyException):
-            _apply([''] + self.lao, diff_text, use_patch=True)
+            _apply([""] + self.lao, diff_text, use_patch=True)
 
     def test_diff_rcs(self):
-        with open('tests/casefiles/diff-rcs.diff') as f:
+        with open("tests/casefiles/diff-rcs.diff") as f:
             diff_text = f.read()
 
         new_text = _apply(self.lao, diff_text)
@@ -137,7 +133,7 @@ class ApplyTestSuite(unittest.TestCase):
         self.assertEqual(new_text, self.tzu)
 
     def test_diff_ed(self):
-        with open('tests/casefiles/diff-ed.diff') as f:
+        with open("tests/casefiles/diff-ed.diff") as f:
             diff_text = f.read()
 
         new_text = _apply(self.lao, diff_text)
@@ -147,5 +143,5 @@ class ApplyTestSuite(unittest.TestCase):
         self.assertEqual(new_text, (self.tzu, None))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
